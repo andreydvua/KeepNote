711dce6efc6837f50a61d8644df6ccb79c71f406
andreydvua <andreydvua@users.noreply.github.com>
2017-04-11 20:00:43 +0300

    Change activity class MainActivity.java

--- app/src/main/java/com/avdsoft/keeper/MainActivity.java
+++ app/src/main/java/com/avdsoft/keeper/MainActivity.java
@@ -1,13 +1,30 @@
 package com.avdsoft.keeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import com.avdsoft.keeper.fragments.StartFragment;
 
 public class MainActivity extends AppCompatActivity {
+//    private final int ARRAY_SIZE = 24;
+//    private List<TaskModel> taskModels = new ArrayList<>();
+//    private ListView listView;
+    public static ConstraintLayout main_container;
+
+//    private void initArrayModel(int size) {
+//        for (int i = 0; i < size; i++) {
+//            taskModels.add(new TaskModel(i + " : 00 ", " test" + i));
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main_container = (ConstraintLayout) findViewById(R.id.main_wrapper);
+        new StartFragment().replaceFragment(this, main_container);
     }
+
+
 }
